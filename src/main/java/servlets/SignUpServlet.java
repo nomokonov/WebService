f9b1147ccc56fe93e3d7e68f19988ab61248791b
Vladimir Nomokonov <vladimir.nomokonov@yandex.ru>
2019-02-08 00:03:29 +0900

    first commit

---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SignUpServlet extends HttpServlet {
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //sign UP
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        UserProfile profile = new UserProfile(login,pass,"email");
+        accountService.addNewUser(profile );
+//        accountService.addSession(request.getSession().getId(), profile);
+        response.setContentType("text/html;charset=utf-8");
+//        response.getWriter().println("Register:" + profile.getLogin());
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
